230eae61d6325008dce837c90c53b04ed9810b8a
FaradillaRoudhotulSanaa <faradillaroudhotul@gmail.com>
2024-03-11 12:48:21 +0700

    Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,30 +47,60 @@ class UserController extends Controller {
         // $user = UserModel::where('username', 'manager')->firstOrFail();
         // return view('user', ['data' => $user]);
 
-        // $user = UserModel::where('level_id', 2)->count();
+        // prak 2.3
+        $user = UserModel::where('level_id', 2)->count();
         // dd($user);
-        // return view('user', ['data' => $user]);
+        return view('user', ['data' => $user]);
 
-        // $user = UserModel::firstOrCreate(
-        //     [
-        //         'username' => 'manager22',
-        //         'nama' => 'Manager',
-        //         'password' => Hash::make('12345'),
-        //         'level_id' => 2
-        //     ],
-        // );
-        // return view('user', ['data' => $user]);
-
-        $user = UserModel::firstOrNew(
+        $user = UserModel::firstOrCreate(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager22',
+                'nama' => 'Manager',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ],
         );
-        $user->save();
         return view('user', ['data' => $user]);
 
+        // $user = UserModel::firstOrNew(
+        //     [
+        //         'username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ],
+        // );
+        // $user->save();
+        // return view('user', ['data' => $user]);
+
+        // prak 2.5 
+        // $user = UserModel::create([
+        //     'username' => 'manager55',
+        //     'nama' => 'Manager55',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2,
+        // ]);
+        // $user->username = 'manager56';
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty(['nama', 'username']); // true
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean(['nama', 'username']); // false
+
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
     }
+
+    // public function tambah() {
+    //     return view('user_tambah');
+    // }
 }
